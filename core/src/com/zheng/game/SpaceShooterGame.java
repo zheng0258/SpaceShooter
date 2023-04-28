482697c06648f9dde35111b1afdb42c6129ccb32
zheng0258 <cubic0258@gmail.com>
2023-04-27 20:01:38 -0700

    laser and input

--- core/src/com/zheng/game/SpaceShooterGame.java
+++ core/src/com/zheng/game/SpaceShooterGame.java
@@ -1,31 +1,32 @@
 package com.zheng.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SpaceShooterGame extends Game {
-    GameScreen gameScreen;
 
-    @Override
-    public void create() {
-        gameScreen = new GameScreen();
-        setScreen(gameScreen);
-    }
-    @Override
-    public void dispose() {
-        gameScreen.dispose();
-    }
+	GameScreen gameScreen;
 
-    @Override
-    public void render() {
-        super.render();
-    }
+	@Override
+	public void create() {
+		gameScreen = new GameScreen();
+		setScreen(gameScreen);
+	}
 
-    @Override
-    public void resize(int width, int height) {
-        gameScreen.resize(width, height);
-    }
+
+	@Override
+	public void dispose() {
+		gameScreen.dispose();
+	}
+
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+
+	@Override
+	public void resize(int width, int height) {
+		gameScreen.resize(width, height);
+	}
 }
